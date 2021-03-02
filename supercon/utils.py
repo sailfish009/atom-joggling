75bb245375ae0107b529934188bd18fd683331d9
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-03-02 11:21:24 +0100

    add short options for some CLI args

--- supercon/utils.py
+++ supercon/utils.py
@@ -21,55 +21,56 @@ def save_checkpoint(state: dict, is_best: bool, path: str) -> None:
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
-    "--task",
-    default="classification",
-    type=str,
-    help="'regression' or 'classification'",
+    "-t", "--task", choices=("regression", "classification"), default="classification"
 )
 parser.add_argument(
     "--csv-path",
     default="data/supercon/labeled.csv",
-    type=str,
-    help="Path relative to project root to the CSV file holding the training data",
+    help="Relative path to CSV file with training data",
 )
 parser.add_argument(
-    "--epochs", default=10, type=int, help="number of total epochs to run"
+    "-e", "--epochs", type=int, default=10, help="number of epochs to run"
 )
 parser.add_argument(
     "--start-epoch",
-    default=0,
     type=int,
-    help="manual epoch number (useful on restarts)",
+    default=0,
+    help="manual starting epoch (useful on restarts)",
 )
-parser.add_argument("--batch-size", default=128, type=int, help="train batchsize")
 parser.add_argument(
     "--bootstrap-idx",
-    default=0,
     type=int,
+    default=0,
     help="index of sample to leave out of training set",
 )
 parser.add_argument(
-    "--lr", "--learning-rate", default=1e-3, type=float, help="initial learning rate"
+    "--learning-rate", type=float, default=1e-3, help="initial learning rate"
 )
 # Checkpoints
 parser.add_argument(
-    "--resume", default="", type=str, help="path to latest checkpoint (default: none)"
+    "--resume",
+    default="",
+    help="path to latest checkpoint (default: none)",
 )
 parser.add_argument(
     "--robust", action="store_true", help="Whether to use a heteroscedastic loss"
 )
 parser.add_argument(
-    "--verbose", action="store_true", help="Whether to print metrics during training"
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="Whether to print metrics while training",
 )
 
-parser.add_argument("--out-dir", default="", help="Directory to output the result")
+parser.add_argument("--out-dir", default="", help="Directory for saving results")
 
-# joggle was found to be optimal at 0.03 Angstrom (https://arxiv.org/abs/2012.02920)
-# anything larger than 0.1 probably doesn't make sense given typical bond
-# lengths of 1 - 2 Angstroms in typical crystals
+# Joggles the position of atoms in a crystal lattice.
+# Was found to be optimal at 0.03 Angstrom (https://arxiv.org/abs/2012.02920)
+# Anything larger than 0.1 probably doesn't make sense given typical bond
+# lengths of 1 - 2 Angstroms in typical crystals.
 parser.add_argument(
     "--joggle",
-    default=0,
     type=float,
+    default=0,
     help="how many Angstroms to randomly perturb atom positions in a crystal",
 )
